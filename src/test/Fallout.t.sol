cec70046ea1c4cb7e235e31d348469a839f882a5
oba <obatirou@gmail.com>
2023-04-13 18:58:48 +0200

    Level 2 Fallout: contracts

---
+++ src/test/Fallout.t.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import "forge-std/Test.sol";
+
+import "../levels/02-Fallout/FalloutFactory.sol";
+import "../core/Ethernaut.sol";
+
+contract FalloutTest is Test {
+    Ethernaut ethernaut;
+    address attacker = makeAddr("attacker");
+
+    function setUp() public {
+        ethernaut = new Ethernaut();
+        vm.deal(attacker, 1 ether);
+    }
+
+    function testFalloutHack() public {
+        /////////////////
+        // LEVEL SETUP //
+        /////////////////
+        FalloutFactory falloutFactory = new FalloutFactory();
+        ethernaut.registerLevel(falloutFactory);
+        vm.startPrank(attacker);
+        address levelAddress = ethernaut.createLevelInstance(falloutFactory);
+        Fallout ethernautFallout = Fallout(payable(levelAddress));
+
+        //////////////////
+        // LEVEL ATTACK //
+        //////////////////
+
+        //////////////////////
+        // LEVEL SUBMISSION //
+        //////////////////////
+        bool levelSuccessfullyPassed = ethernaut.submitLevelInstance(payable(levelAddress));
+        vm.stopPrank();
+        assert(levelSuccessfullyPassed);
+    }
+}
